a8de6cacf6ae1f243cff5f6b667d30e4c4d5e9fa
coreybutler <=corey@coreybutler.cmom>
2014-04-23 10:49:03 -0500

    Fixed version command

--- bin/commands/list.js
+++ bin/commands/list.js
@@ -6,30 +6,30 @@ var cli = require('optimist'),
 require('colors');
 
 var timeout = setTimeout(function(){
-	console.log('Fenix did not respond (timeout)'.magenta.bold);
-	process.exit(1);
-},3500);
+  console.log('Fenix did not respond (timeout)'.magenta.bold);
+  process.exit(1);
+},13500);
 
 request.get('http://127.0.0.1:33649/server/list',function(err,res,body){
-	clearTimeout(timeout);
-	if (err){
-		if (err.message.indexOf('ECONNREFUSED') >= 0){
-			console.log('Fenix is not currently running or could not be reached.'.red.bold);
-			return;
-		} else {
-			throw err;
-		}
-	}
+  clearTimeout(timeout);
+  if (err){
+    if (err.message.indexOf('ECONNREFUSED') >= 0){
+      console.log('Fenix is not currently running or could not be reached.'.red.bold);
+      return;
+    } else {
+      throw err;
+    }
+  }
 
-	console.log('\n  SERVERS:\n'.bold);
-	JSON.parse(body).forEach(function(site){
-		var ttl = site.name;
-		ttl = (site.running ? (ttl+' [on]').green : ttl+' [off]'.grey).bold;// + (' (ID: '+site.id+')').grey;
-		console.log('  '+ttl);
-		console.log('   - '+(site.path).grey);
-		console.log('   - '+('http://127.0.0.1:'+site.port.toString())[(site.running?'green':'red')]);
-		if (site.publicUrl){
-			console.log('   - '+site.publicUrl.cyan.bold);
-		}
-	});
+  console.log('\n  SERVERS:\n'.bold);
+  JSON.parse(body).forEach(function(site){
+    var ttl = site.name;
+    ttl = (site.running ? (ttl+' [on]').green : ttl+' [off]'.grey).bold;// + (' (ID: '+site.id+')').grey;
+    console.log('  '+ttl);
+    console.log('   - '+(site.path).grey);
+    console.log('   - '+('http://127.0.0.1:'+site.port.toString())[(site.running?'green':'red')]);
+    if (site.publicUrl){
+      console.log('   - '+site.publicUrl.cyan.bold);
+    }
+  });
 });
--- bin/commands/version.js
+++ bin/commands/version.js
@@ -1,21 +1,26 @@
 var p = require('path'),
   pkg = require(p.join(__dirname,'..','..','package.json')),
   fs = require('fs'),
-	request = require('request');
+  request = require('request');
 
 require('colors');
 
+var timeout = setTimeout(function(){
+  console.log('Fenix did not respond (timeout)'.magenta.bold);
+  process.exit(1);
+},3500);
+
 request.get('http://127.0.0.1:33649/version',function(err,res,body){
-	console.log('');
-	if (err){
-		if (err.message.indexOf('ECONNREFUSED') >= 0){
-			console.log('Fenix is not currently running or could not be reached.'.red.bold);
-		} else {
-			throw err;
-		}
-	} else {
-		console.log(('  Fenix v'+body).bold);
-	}
-	
-	console.log(('  Fenix CLI v'+pkg.version).bold);
+  clearTimeout(timeout);
+  console.log('');
+  if (err){
+    if (err.message.indexOf('ECONNREFUSED') >= 0){
+      console.log('Fenix is not currently running or could not be reached.'.red.bold);
+    } else {
+      throw err;
+    }
+  } else {
+    console.log(('  Fenix v'+body).bold);
+    console.log(('  Fenix CLI v'+pkg.version).bold);
+  }
 });
